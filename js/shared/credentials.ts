38b45c3c30a65bb76dcec624c6083bf74996001c
ElectricEd1813 <electric@dynacheminc.com>
2024-01-11 08:31:18 -0600

    Update credentials.ts

--- js/shared/credentials.ts
+++ js/shared/credentials.ts
@@ -2,8 +2,8 @@
 // !!! In a real application, those informations would be kept in a database table
 // !!! Do NOT commit your secrets.
 
-const clientID = "YOUR_APP_ID";
-const clientSecret = "YOUR_APP_SECRET";
+const clientID = 18W7qLKhD-0YK4fvVaEbzZetlp5SNH3GsN2Zgy3XwOo;
+const clientSecret = RIdcu9mjI8FQGkoA5xkuTMUsqkt52AYlRNsfSAgBCsg;
 const scopes = "read+write";
 
 export { clientID, clientSecret, scopes };
